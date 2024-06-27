c9836336d5ad8517f460ebf4f1006273a7e9b5c9
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-06-27 14:24:06 +0300

    the project file

---
+++ ecommerce_mobile/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(e_commerce());
+}
+
+class e_commerce extends StatelessWidget {
+  const e_commerce({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("data"));
+  }
+}
