8c2b4ac84a9340a425c6e085ba07adeae3c94c96
Mohamed El Dahtowry <salintofficial@hotmail.com>
2024-06-27 10:09:17 +0300

    Add style and logo

--- ecommerce-frontend/src/index.js
+++ ecommerce-frontend/src/index.js
@@ -1,4 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<h1>Ecommerce</h1>);
+import "./style.css";
+
+const App = () => {
+
+	const router = createBrowserRouter([
+		{
+			path: "/auth/signup",
+			element: <h1>Hello, world</h1>
+		}
+	]);
+
+	return <RouterProvider router={router} />
+
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
