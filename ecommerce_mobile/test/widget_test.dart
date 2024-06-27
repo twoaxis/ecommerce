b97c07c3893eed2db7de61697a383b7b656d19c4
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-06-27 14:51:08 +0300

    files project mobile

--- ecommerce_mobile/test/widget_test.dart
+++ ecommerce_mobile/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ecommerce_mobile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const e_commerce());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
