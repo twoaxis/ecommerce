0bd773a0b15dbae62abc0a4df4ede858dbd65846
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-06-29 01:24:54 +0300

    Register endpoint

---
+++ ecommerce_backend/API/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using API.Dtos;
+using Core.Entities.IdentityEntities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseController
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpPost("register")]
+        [ProducesResponseType(typeof(AppUserDto), StatusCodes.Status200OK)]
+        //[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<AppUserDto>> Register(RegisterDto model)
+        {
+            //if (CheckEmailExist(model.Email).Result.Value)
+            //    return BadRequest(new ApiValidationErrorResponse() { Errors = new string[] { "This email has already been used" } });
+
+            var user = new AppUser()
+            {
+                DisplayName = model.DisplayName,
+                Email = model.Email,
+                UserName = model.Email.Split('@')[0],
+                PhoneNumber = model.PhoneNumber,
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            //if (result.Succeeded is false)
+            //    return BadRequest(new ApiResponse(400));
+
+            return Ok(new AppUserDto
+            {
+                DisplayName = user.DisplayName,
+                Email = model.Email,
+                //Token = await _authService.CreateTokenAsync(user, _userManager)
+            });
+        }
+
+
+        [HttpGet("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmailExist(string email)
+        {
+            return await _userManager.FindByEmailAsync(email) is not null;
+        }
+    }
+}
