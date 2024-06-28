51e6aa60e2dc939f3409833e15ff3bb777e7f6e5
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-06-28 18:52:42 +0300

    Fix overlapping issue

--- ecommerce_mobile/lib/pages/log_in.dart
+++ ecommerce_mobile/lib/pages/log_in.dart
@@ -11,86 +11,88 @@ class LogIn extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(33.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                "asset/image/logo.png",
-                width: MediaQuery.of(context).size.width -
-                    MediaQuery.of(context).size.width * .25,
-              ),
-              Text(
-                "Log in to your account",
-                style: TextStyle(
-                  fontFamily: "Roboto",
-                  fontSize: 40,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  "asset/image/logo.png",
+                  width: MediaQuery.of(context).size.width -
+                      MediaQuery.of(context).size.width * .25,
                 ),
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              CustomTextField.CustomTextField(
-                text: "E-mail",
-                textInputType: TextInputType.emailAddress,
-                hint_text: 'Enter Your Email',
-                isPassword: false,
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              CustomTextField.CustomTextField(
-                text: "Password",
-                textInputType: TextInputType.emailAddress,
-                hint_text: 'Enter Your Password',
-                isPassword: true,
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Log in",
-                    style: TextStyle(
-                      fontFamily: "Roboto",
-                      fontSize: 20,
-                      color: Colors.white,
-                    )),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFa71f1f),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
+                Text(
+                  "Log in to your account",
+                  style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontSize: 40,
                   ),
-                  fixedSize: Size(double.maxFinite, 50),
                 ),
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              Text(
-                'By logging in, you agree to our terms of conditions and privacy policy',
-                style: TextStyle(
-                  fontFamily: "Roboto",
-                  fontSize: 20,
-                  color: Colors.black38,
+                SizedBox(
+                  height: 33.0,
                 ),
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  'Not a member? Sign up',
+                CustomTextField.CustomTextField(
+                  text: "E-mail",
+                  textInputType: TextInputType.emailAddress,
+                  hint_text: 'Enter Your Email',
+                  isPassword: false,
+                ),
+                SizedBox(
+                  height: 33.0,
+                ),
+                CustomTextField.CustomTextField(
+                  text: "Password",
+                  textInputType: TextInputType.emailAddress,
+                  hint_text: 'Enter Your Password',
+                  isPassword: true,
+                ),
+                SizedBox(
+                  height: 33.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Log in",
+                      style: TextStyle(
+                        fontFamily: "Roboto",
+                        fontSize: 20,
+                        color: Colors.white,
+                      )),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFa71f1f),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    fixedSize: Size(double.maxFinite, 50),
+                  ),
+                ),
+                SizedBox(
+                  height: 33.0,
+                ),
+                Text(
+                  'By logging in, you agree to our terms of conditions and privacy policy',
                   style: TextStyle(
                     fontFamily: "Roboto",
                     fontSize: 20,
                     color: Colors.black38,
                   ),
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 33.0,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'Not a member? Sign up',
+                    style: TextStyle(
+                      fontFamily: "Roboto",
+                      fontSize: 20,
+                      color: Colors.black38,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- ecommerce_mobile/lib/pages/sign_up.dart
+++ ecommerce_mobile/lib/pages/sign_up.dart
@@ -12,96 +12,99 @@ class Sign_up extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(33.0),
-          child: Column(
-            children: [
-              Image.asset(
-                "asset/image/logo.png",
-                width: MediaQuery.of(context).size.width -
-                    MediaQuery.of(context).size.width * .25,
-              ),
-              Text(
-                "Create an Account",
-                style: TextStyle(
-                  fontFamily: "Roboto",
-                  fontSize: 40,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset(
+                  "asset/image/logo.png",
+                  width: MediaQuery.of(context).size.width -
+                      MediaQuery.of(context).size.width * .25,
                 ),
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              CustomTextField.CustomTextField(
-                text: "E-mail",
-                textInputType: TextInputType.emailAddress,
-                hint_text: 'johnsmith@twoaxis.com',
-                isPassword: false,
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              CustomTextField.CustomTextField(
-                text: "Password",
-                textInputType: TextInputType.emailAddress,
-                hint_text: 'Enter Your Password',
-                isPassword: true,
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              CustomTextField.CustomTextField(
-                text: "Confirm Password",
-                textInputType: TextInputType.emailAddress,
-                hint_text: 'Enter Your Password',
-                isPassword: true,
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Sign Up",
-                    style: TextStyle(
-                      fontFamily: "Roboto",
-                      fontSize: 20,
-                      color: Colors.white,
-                    )),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFa71f1f),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
+                Text(
+                  "Create an Account",
+                  style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontSize: 40,
                   ),
-                  fixedSize: Size(double.maxFinite, 50),
                 ),
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              Text(
-                'By signing up, you agree to our terms of conditions and privacy policy',
-                style: TextStyle(
-                  fontFamily: "Roboto",
-                  fontSize: 20,
-                  color: Colors.black38,
+                SizedBox(
+                  height: 33.0,
                 ),
-              ),
-              SizedBox(
-                height: 33.0,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return LogIn();
-                  }));
-                },
-                child: Text(
-                  'Already have an account? Log In',
+                CustomTextField.CustomTextField(
+                  text: "E-mail",
+                  textInputType: TextInputType.emailAddress,
+                  hint_text: 'johnsmith@twoaxis.com',
+                  isPassword: false,
+                ),
+                SizedBox(
+                  height: 33.0,
+                ),
+                CustomTextField.CustomTextField(
+                  text: "Password",
+                  textInputType: TextInputType.emailAddress,
+                  hint_text: 'Enter Your Password',
+                  isPassword: true,
+                ),
+                SizedBox(
+                  height: 33.0,
+                ),
+                CustomTextField.CustomTextField(
+                  text: "Confirm Password",
+                  textInputType: TextInputType.emailAddress,
+                  hint_text: 'Enter Your Password',
+                  isPassword: true,
+                ),
+                SizedBox(
+                  height: 33.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Sign Up",
+                      style: TextStyle(
+                        fontFamily: "Roboto",
+                        fontSize: 20,
+                        color: Colors.white,
+                      )),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFa71f1f),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    fixedSize: Size(double.maxFinite, 50),
+                  ),
+                ),
+                SizedBox(
+                  height: 33.0,
+                ),
+                Text(
+                  'By signing up, you agree to our terms of conditions and privacy policy',
                   style: TextStyle(
                     fontFamily: "Roboto",
                     fontSize: 20,
                     color: Colors.black38,
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 33.0,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return LogIn();
+                    }));
+                  },
+                  child: Text(
+                    'Already have an account? Log In',
+                    style: TextStyle(
+                      fontFamily: "Roboto",
+                      fontSize: 20,
+                      color: Colors.black38,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
