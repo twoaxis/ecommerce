35f4cde24465141cae437d97df4e29c3411c6b84
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-07-01 23:09:56 +0300

    Role manager controller

---
+++ ecommerce_backend/API/Controllers/RoleController.cs
@@ -0,0 +1,49 @@
+﻿using API.Dtos;
+using API.Errors;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class RoleController : BaseController
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<RoleDto>>> GetAllRoles()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            var rolesDto = new List<RoleDto>();
+            foreach (var role in roles)
+            {
+                rolesDto.Add(new RoleDto()
+                {
+                    Id = role.Id,
+                    Name = role.Name
+                });
+            }
+
+            return Ok(rolesDto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RoleDto>> CreateRole(string RoleName)
+        {
+            var roleExist = await _roleManager.RoleExistsAsync(RoleName);
+            if(!roleExist)
+            {
+                await _roleManager.CreateAsync(new IdentityRole(RoleName));
+                var role = await _roleManager.Roles.Where(O => O.Name == RoleName).FirstOrDefaultAsync();
+                return Ok(new RoleDto() { Id = role.Id, Name = role.Name});
+            }
+
+            return BadRequest(new ApiResponse(400, "This role is already exist!"));
+        }
+    }
+}
---
+++ ecommerce_backend/API/Dtos/RoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Dtos
+{
+    public class RoleDto
+    {
+        public string Id { get; set; } 
+        public string Name { get; set; } 
+    }
+}
