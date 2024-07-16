604a4e56eefb33ce88836b4474ad76688b8ca704
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-07-16 23:20:29 +0300

    Specifications design pattern

--- ecommerce_backend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ ecommerce_backend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b7769e4aac7704e362a7987a300638e7d7b68a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1355271c8cb8b960b445f72b4835eaf1b06d39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
