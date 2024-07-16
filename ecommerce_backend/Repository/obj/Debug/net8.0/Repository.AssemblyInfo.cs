a3b29d4d63d8fbf85c5014a868c7535df6fab8c6
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-07-16 23:52:38 +0300

    Unit of Work design pattern

--- ecommerce_backend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ ecommerce_backend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1355271c8cb8b960b445f72b4835eaf1b06d39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604a4e56eefb33ce88836b4474ad76688b8ca704")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
