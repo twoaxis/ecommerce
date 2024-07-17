34f3f8f7828db8025cc70fa15c03e8a56d1539be
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-07-18 00:06:21 +0300

    Add Category Service

--- ecommerce_backend/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ ecommerce_backend/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9777512fa7721bf70b027b42f046a9925d8eb308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b1a8317973a7ebcf0d4fc2552ec0c13a65a574")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ecommerce_backend/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ ecommerce_backend/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9777512fa7721bf70b027b42f046a9925d8eb308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b1a8317973a7ebcf0d4fc2552ec0c13a65a574")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
